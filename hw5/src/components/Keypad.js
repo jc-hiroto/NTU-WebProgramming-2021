7530487b9b0c28201c4d43e161621031840e98b2
jc-hiroto <taste.ful.msa@gmail.com>
2021-11-07 15:38:18 +0800

    Finished advanced functions.

--- hw5/src/components/Keypad.js
+++ hw5/src/components/Keypad.js
@@ -1,5 +1,6 @@
 function Keypad(props){
     const numkeys = [
+        ['(', ')', 'e'],
         ['7', '8', '9'],
         ['4', '5', '6'],
         ['1', '2', '3'],
@@ -34,16 +35,37 @@ function Keypad(props){
         }
         if(key === "AC"){
             props.setdispNum("0");
+            props.setOperation("");
+            props.setError("");
             return;
         }
-        if(props.operator){
-            props.setdispNum(key);
-            props.setOperator("");
+        if(key === "="){
+            if(!props.dispNum.match(/[(e+\-*\/^)]/)){
+                return;
+            }
+            props.setOperation(props.dispNum);
+            try{
+                var ans = eval(props.dispNum.replace("^","**"));
+            }catch(e){
+                props.setError(e.message);
+                return;
+            }
+            if(!props.error){
+                let exp = Math.floor(Math.log(ans)/Math.log(10));
+                console.log(exp);
+                console.log(ans/(10**exp));
+                if(exp > 9){
+                    ans = ans/(10**exp)+"e"+exp;
+                }
+                props.setdispNum(ans.toString());
+                return;
+            }
         }
         else if(props.dispNum === '0'){
             if(key === '.'){
                 props.setdispNum("0.")
-            }else{
+            }
+            else{
                 props.setdispNum(key);
             }
         }
@@ -51,13 +73,17 @@ function Keypad(props){
             props.setdispNum(props.dispNum + key);
         }
     }
-    const handle_operator_keypress = (e) => {
-        const key = e.target.id;
-        console.log(key);
+    const handleRecall = (num) => {
+        console.log(num);
+        if(props.dispNum === '0'){
+            props.setdispNum(num);
+        }else{
+            props.setdispNum(props.dispNum + num);
+        }
     }
     const renderKeypad = () => {
         return (
-            <div className="grid-cols-3 grid-rows-4">
+            <div className="grid grid-cols-3 w-64 h-96 justify-self-center ml-5 mr-5">
             {numkeys.map((row, i) => {
                 return (
                             row.map((numkey) => {
@@ -74,14 +100,12 @@ function Keypad(props){
     }
     const renderOperatorKeypad = () => {
         return (
-            <div className="">
+            <div className="grid grid-cols-1">
             {operator_keys.map((key) => {
                 return (
-                    <div>
-                        <button className="calc-keypad-btn justify-center m-1" id={key.id} onClick={handle_operator_keypress}>
-                            <i className={key.icon + " self-center"}></i>
-                        </button>
-                    </div>
+                    <button className="calc-keypad-btn justify-center m-1" id={key.id} onClick={handle_num_keypress}>
+                        <i className={key.icon + " self-center"} id={key.id}></i>
+                    </button>
 
                 )
             })}
@@ -89,9 +113,31 @@ function Keypad(props){
             </div>
         )
     }
+    const renderNumHist = () => {
+        return props.numMem.map((num, i) => {
+            return (
+                <div className="flex flex-row mt-1 mb-1 h-10 w-40 bg-indigo-100 hover:bg-indigo-300 rounded-xl justify-center self-center cursor-pointer" key={i} onClick={() => handleRecall(num)}>
+                    <i className="fas fa-history fa-lg self-center"></i>
+                    <p className="self-center font-bold text-xl text-indigo-800 ml-2">{num}</p>
+                </div>
+            )
+        })
+    }
     return (
-        <div className="grid grid-cols-3">
-            <div></div>
+        <div className="flex flex-row justify-center">
+            <div className="flex flex-col justify-start">
+                <div className="flex flex-row justify-center self-center mr-1 ml-1">
+                    <button className="calc-keypad-btn justify-center m-1" id="btn-save" onClick={props.handleSaveNum}>
+                        <i className="fas fa-file-import self-center"></i>
+                    </button>
+                    <button className="calc-keypad-btn justify-center m-1" id="btn-clear-mem" onClick={props.handleClearMem}>
+                        <i className="fas fa-eraser self-center"></i>
+                    </button>
+                </div>
+                <div className="flex flex-col">
+                    {renderNumHist()}
+                </div>
+            </div>
             {renderKeypad()}
             {renderOperatorKeypad()}
         </div>
--- hw5/src/components/NumDisplay.js
+++ hw5/src/components/NumDisplay.js
@@ -1,10 +1,29 @@
 function NumDisplay(props) {
-  return (
-    <div className="grid">
-        <div className="num-display-container">
-            <p className="num-display-text">{props.num}</p>
+    const render_error_msg = () => {
+        if (props.error) {
+            console.log(props.error);
+            return (
+                <div class="text-center py-4 lg:px-4">
+                    <div class="p-2 bg-red-800 items-center text-red-100 leading-none rounded-full inline-flex" role="alert">
+                        <span class="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">Error</span>
+                        <span class="font-semibold mr-2 text-left flex-auto">{props.error}</span>
+                        </div>
+                </div>
+            )
+        }
+    }
+    return (
+        <div className="grid">
+            {render_error_msg()}
+            <div className="flex flex-row justify-center">
+                <i className="fas fa-undo self-center fa-lg hover:text-indigo-600 hover:scale-105" onClick={props.handleUndo}/>
+                <div className="num-display-container flex flex-col">
+                    <p className="num-hist-display-text">{props.operation + " ="}</p>
+                    <p className="num-display-text">{props.error === "" ? props.dispNum : "ERR"}</p>
+                </div>
+                <i className="fas fa-backspace self-center fa-lg hover:text-indigo-600 hover:scale-105" onClick={props.handleBackspace}/>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 export default NumDisplay;
