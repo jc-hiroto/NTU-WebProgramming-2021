0b663aab8af5046f993188395819d85ab313f493
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-07 10:30:47 +0800

    [hack2] Completed features (untested.)

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,87 @@
+import express from 'express'
+import Post from '../models/post'
+import moment from 'moment'
+
+const router = express.Router()
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+router.get('/allPosts', async(req, res) => {
+    await Post.find({}, (err, posts) => {
+        // sort by timestamp
+        posts.sort((a, b) => {
+            return moment(b.timestamp).diff(moment(a.timestamp))
+        })
+        if (err) {
+            res.status(403).json({
+                message: "error",
+                data: null
+            })
+        } else {
+            res.status(200).json({
+                message: 'success',
+                data: posts
+            })
+        }
+    })
+});
+
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+router.get('/postDetail', async(req, res) => {
+    const postId = req.query.postId;
+    await Post.find({"postId": postId}, (err, post) => {
+        if (err) {
+            res.status(403).json({
+                message: "error",
+                data: null
+            })
+        } else {
+            res.status(200).json({
+                message: 'success',
+                data: post
+            })
+        }
+    });
+});
+
+// TODO 4-(1): create the 3rd API (/api/newPost)
+router.post('/newPost', async(req, res) => {
+    console.log(req.body);
+    const post = new Post({
+        postId: req.body.postId,
+        title: req.body.title,
+        content: req.body.content,
+        timestamp: req.body.timestamp
+    });
+    await post.save((err, post) => {
+        if (err) {
+            res.status(403).json({
+                message: "error",
+                data: null
+            })
+        } else {
+            res.status(200).json({
+                message: 'success',
+                data: post
+            })
+        }
+    });
+});
+
+// TODO 5-(1): create the 4th API (/api/post)
+// delete one post by postId
+router.delete('/post', async(req, res) => {
+    const postId = req.query.postId;
+    await Post.deleteOne({"postId": postId}, (err, post) => {
+        if (err) {
+            res.status(403).json({
+                message: "error",
+            })
+        } else {
+            res.status(200).json({
+                message: 'success',
+            })
+        }
+    });
+});
+
+export default router
---
+++ hack2/src/edit.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import instance from './instance'
+
+import { Button, TextField } from '@material-ui/core'
+import { Delete as DeleteIcon, Send as SendIcon } from '@material-ui/icons'
+import { v4 as uuidv4 } from 'uuid'
+
+function Edit(props) {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  // TODO 4-(2): complete handleSubmit function to create a new post and save it to database 
+  const handleSubmit = async() => {
+    console.log(title, content)
+    const resp = await instance.post('/newPost', {
+      title: title,
+      content: content,
+      postId: uuidv4(),
+      timestamp: Date.now()
+    });
+    console.log(resp)
+    setTimeout(() => {
+      props.navigate(-1);
+    }, 300)
+  }
+
+  return (
+    <div className="post-wrapper">
+      <div className="post-text-container">
+        <div style={{ fontWeight: 'Bold', fontSize: 18 }}>Create a New Post</div>
+
+        <div className="post-title">
+          <TextField
+            label="Title"
+            value={title}
+            size="small"
+            variant="outlined"
+            className="post-title"
+            id="pid-create-title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="post-content-container">
+          <TextField
+            label="Content"
+            variant="outlined"
+            className="post-content-editor"
+            id="pid-create-content"
+            onChange={(e) => setContent(e.target.value)}
+            multiline
+          />
+        </div>
+
+        <div className="post-btn-wrapper">
+          <Button variant="contained" color="primary" className="post-btn" startIcon={<SendIcon />} id="pid-create-submit-btn" onClick={handleSubmit}>Submit</Button>
+          <Button variant="contained" color="secondary" className="post-cancel-btn" endIcon={<DeleteIcon />} onClick={e => props.navigate(-1)}>Cancel</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Edit
